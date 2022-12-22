4f1973c1c7547bf9c87f053e0012727a9b7b3645
Tom <tflo@users.noreply.github.com>
2022-12-22 07:57:33 +0100

    Style

--- main.lua
+++ main.lua
@@ -1,40 +1,40 @@
-local addon_name, a = ...
+local addonName, a = ...
 
-aqt_global_db = aqt_global_db or {}
-aqt_char_db = aqt_char_db or {}
+AQT_GlobalDB = AQT_GlobalDB or {}
+AQT_CharDB = AQT_CharDB or {}
 
 local debug = false
-local AQT_MESSAGE_PREFIX = "|cff2196f3Auto Quest Tracker|r: "
+local MSG_PREFIX = '|cff2196f3Auto Quest Tracker|r: '
 
-local function print_loadmsg(msg)
-	if a.gdb.loadmsg then C_Timer.After(8, function() print(AQT_MESSAGE_PREFIX .. msg) end) end
+local function printLoadMsg(msg)
+	if a.gdb.loadMsg then C_Timer.After(8, function() print(MSG_PREFIX .. msg) end) end
 end
 
-local f = CreateFrame("Frame")
-f:RegisterEvent("ADDON_LOADED")
+local f = CreateFrame 'Frame'
+f:RegisterEvent 'ADDON_LOADED'
 
-local function register_all_events()
-	f:RegisterEvent("ZONE_CHANGED")
-	f:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+local function registerAllEvents()
+	f:RegisterEvent 'ZONE_CHANGED'
+	f:RegisterEvent 'ZONE_CHANGED_NEW_AREA'
 end
 
-local function unregister_all_events()
-	f:UnregisterEvent("ZONE_CHANGED")
-	f:UnregisterEvent("ZONE_CHANGED_NEW_AREA")
+local function unregisterAllEvents()
+	f:UnregisterEvent 'ZONE_CHANGED'
+	f:UnregisterEvent 'ZONE_CHANGED_NEW_AREA'
 end
 
-local function on_event(self, event, addon)
-	if event == "ADDON_LOADED" then
-		if addon == addon_name then
-			f:UnregisterEvent("ADDON_LOADED")
-			a.gdb, a.cdb = aqt_global_db, aqt_char_db
+local function onEvent(self, event, addon)
+	if event == 'ADDON_LOADED' then
+		if addon == addonName then
+			f:UnregisterEvent 'ADDON_LOADED'
+			a.gdb, a.cdb = AQT_GlobalDB, AQT_CharDB
 			a.cdb.enabled = a.cdb.enabled == nil and true or a.cdb.enabled
-			a.gdb.loadmsg = a.gdb.loadmsg == nil and true or a.gdb.loadmsg
+			a.gdb.loadMsg = a.gdb.loadMsg == nil and true or a.gdb.loadMsg
 			if a.cdb.enabled then
-				register_all_events()
-				print_loadmsg('Enabled')
+				registerAllEvents()
+				printLoadMsg 'Enabled'
 			else
-				print_loadmsg('Disabled')
+				printLoadMsg 'Disabled'
 			end
 		end
 	else
@@ -42,51 +42,62 @@ local function on_event(self, event, addon)
 	end
 end
 
-
-SLASH_AUTOQUESTTRACKER1 = "/autoquesttracker"
-SLASH_AUTOQUESTTRACKER2 = "/aqt"
-SlashCmdList["AUTOQUESTTRACKER"] = function(msg)
-	if msg == "on" then
-		register_all_events()
-		print(AQT_MESSAGE_PREFIX .. "Enabled")
+SLASH_AUTOQUESTTRACKER1 = '/autoquesttracker'
+SLASH_AUTOQUESTTRACKER2 = '/aqt'
+SlashCmdList['AUTOQUESTTRACKER'] = function(msg)
+	if msg == 'on' then
+		registerAllEvents()
+		print(MSG_PREFIX .. 'Enabled')
 		a.cdb.enabled = true
-	elseif msg == "off" then
-		unregister_all_events()
-		print(AQT_MESSAGE_PREFIX .. "Disabled")
+	elseif msg == 'off' then
+		unregisterAllEvents()
+		print(MSG_PREFIX .. 'Disabled')
 		a.cdb.enabled = false
-	elseif msg == "toggleloadingmessage" then
-		a.gdb.loadmsg = not a.gdb.loadmsg
-		print(AQT_MESSAGE_PREFIX .. "Loading message " .. (a.gdb.loadmsg and "enabled" or "disabled") .. " for all chars")
-	elseif msg == "debug" then
+	elseif msg == 'toggleloadingmessage' then
+		a.gdb.loadMsg = not a.gdb.loadMsg
+		print(MSG_PREFIX .. 'Loading message ' .. (a.gdb.loadMsg and 'enabled' or 'disabled') .. ' for all chars')
+	elseif msg == 'debug' then
 		debug = not debug
-		print(AQT_MESSAGE_PREFIX .. "Debug mode " .. (debug and "enabled" or "disabled"))
-	elseif msg == "quests" then
+		print(MSG_PREFIX .. 'Debug mode ' .. (debug and 'enabled' or 'disabled'))
+	elseif msg == 'quests' then
 		debug = true
-		AQT_PrintDebugMsg("Quests currently in quest log:")
+		AQT_PrintDebugMsg 'Quests currently in quest log:'
 		for questIndex = 1, C_QuestLog.GetNumQuestLogEntries() do
-			local questTitle, isHeader, questId, isWorldQuest, isHidden, isCalling, isOnMap, hasLocalPOI = AQT_getQuestInfo(questIndex)
+			local questTitle, isHeader, questId, isWorldQuest, isHidden, isCalling, isOnMap, hasLocalPOI =
+				AQT_getQuestInfo(questIndex)
 			if isHidden then
-				print(string.format("|cff707070[Hidden] %s (%s)", questTitle, tostring(questId)))
+				print(string.format('|cff707070[Hidden] %s (%s)', questTitle, tostring(questId)))
 			elseif isHeader then
-				print(string.format("[Header] %s", questTitle))
+				print(string.format('[Header] %s', questTitle))
 			elseif isWorldQuest then
-				print(string.format("|cffC107f3[Wolrd Quest] %s (%s)", questTitle, tostring(questId)))
+				print(string.format('|cffC107f3[World Quest] %s (%s)', questTitle, tostring(questId)))
 			elseif isCalling then
-				print(string.format("[Calling quest] %s (%s) %s/%s", questTitle, tostring(questId), tostring(isOnMap), tostring(hasLocalPOI)))
+				print(
+					string.format(
+						'[Calling quest] %s (%s) %s/%s',
+						questTitle,
+						tostring(questId),
+						tostring(isOnMap),
+						tostring(hasLocalPOI)
+					)
+				)
 			else
-				print(string.format("|cfff2cb06%s (%s)", questTitle, tostring(questId)))
+				print(string.format('|cfff2cb06%s (%s)', questTitle, tostring(questId)))
 			end
 		end
 		debug = false
 	else
-		print(AQT_MESSAGE_PREFIX .. "Status: " .. (a.cdb.enabled and "Enabled" or "Disabled") .. ". Available commands are: on, off, debug, quests, toggleloadingmessage")
+		print(
+			MSG_PREFIX
+				.. 'Status: '
+				.. (a.cdb.enabled and 'Enabled' or 'Disabled')
+				.. '. Available commands are: on, off, debug, quests, toggleloadingmessage'
+		)
 	end
 end
 
 function AQT_PrintDebugMsg(msg)
-	if debug then
-		print(AQT_MESSAGE_PREFIX .. msg);
-	end
+	if debug then print(MSG_PREFIX .. msg) end
 end
 
 function AQT_HandleEvent(self, event, ...)
@@ -97,17 +108,15 @@ end
 function AQT_UpdateQuestsForZone()
 	local currentZone = GetRealZoneText()
 	local minimapZone = GetMinimapZoneText()
-	if currentZone == nil and minimapZone == nil then
-		return
-	end
+	if currentZone == nil and minimapZone == nil then return end
 
-
-	AQT_PrintDebugMsg("Updating quests for: " .. currentZone .. " or " .. minimapZone)
+	AQT_PrintDebugMsg('Updating quests for: ' .. currentZone .. ' or ' .. minimapZone)
 
 	local questZone = nil
 
 	for questIndex = 1, C_QuestLog.GetNumQuestLogEntries() do
-		local questTitle, isHeader, questId, isWorldQuest, isHidden, isCalling, isOnMap, hasLocalPOI = AQT_getQuestInfo(questIndex)
+		local questTitle, isHeader, questId, isWorldQuest, isHidden, isCalling, isOnMap, hasLocalPOI =
+			AQT_getQuestInfo(questIndex)
 
 		if not isWorldQuest and not isHidden then
 			if isHeader then
@@ -125,36 +134,33 @@ function AQT_UpdateQuestsForZone()
 	end
 end
 
-
 function AQT_ShowOrHideQuest(questIndex, questId, show)
 	-- Checks that the quest is still in the quest log, and that we are not in combat lockdown to avoid tainting
 	local questTitle, _, id = AQT_getQuestInfo(questIndex)
 	if id == questId and not (InCombatLockdown() == 1) then
 		if show then
-			AQT_PrintDebugMsg(string.format("Tracking: %s (%s)", questTitle, questId))
+			AQT_PrintDebugMsg(string.format('Tracking: %s (%s)', questTitle, questId))
 			C_QuestLog.AddQuestWatch(questId)
 		else
-			AQT_PrintDebugMsg(string.format("Removing: %s (%s)", questTitle, questId))
+			AQT_PrintDebugMsg(string.format('Removing: %s (%s)', questTitle, questId))
 			C_QuestLog.RemoveQuestWatch(questId)
 		end
 	end
 end
 
--- Returns:
---   Quest Title
---   Whether it's a zone header in the quest log
---	 Quest Id
---	 Whether the quest is a world quest
---	 Whether the quest is hidden in the quest log
---   Whether the quest is a calling quest
---	 Whether the quest is on the map
---	 Whether the quest has a local point of interest
 function AQT_getQuestInfo(index)
 	local quest = C_QuestLog.GetInfo(index)
-	return quest.title, quest.isHeader, quest.questID, C_QuestLog.IsWorldQuest(quest.questID), quest.isHidden, C_QuestLog.IsQuestCalling(quest.questID), quest.isOnMap, quest.hasLocalPOI
+	return quest.title,
+		quest.isHeader,
+		quest.questID,
+		C_QuestLog.IsWorldQuest(quest.questID),
+		quest.isHidden,
+		C_QuestLog.IsQuestCalling(quest.questID),
+		quest.isOnMap,
+		quest.hasLocalPOI
 end
 
-f:SetScript("OnEvent", on_event)
+f:SetScript('OnEvent', onEvent)
 
 
 
